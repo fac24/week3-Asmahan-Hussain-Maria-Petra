09c1fb47d4147ca9b14af3beedb67e5331ee1ad2
mariaalouisaa <maria.louisa.castro@gmail.com>
2022-05-26 08:46:55 +0100

    Add form

--- routes/view-posts.js
+++ routes/view-posts.js
@@ -1,36 +1,33 @@
 const db = require("../database/connection");
 
 function showPosts(request, response) {
-  const SELECT_USER = `SELECT users.username, movie_title.posts, comment.posts, rating.posts.
-    FROM users
-    INNER JOIN posts
-    ON users.id = posts.user_id`;
-  db.query(SELECT_USER).then((result) => {
-    console.log(result);
-    // Assuming below is correct it can be uncommented. Can't test on my laptop with no database setup :'(
+  // const SELECT_USER = `SELECT users.username, movie_title.posts, comment.posts, rating.posts.
+  //   FROM users
+  //   INNER JOIN posts
+  //   ON users.id = posts.user_id`;
+  // db.query(SELECT_USER).then((result) => {
+  //   console.log(result);
+  // Assuming below is correct it can be uncommented. Can't test on my laptop with no database setup :'(
 
-    // let postsHTML = "";
-    // const posts = result.rows;
-    // posts.map(
-    //   (post) =>
-    //     (postsHTML = `
-    //     <div class="post-container">
-    //     <p>Username: ${post.username}</p>
-    //     <p>Movie: ${post.movie_title}</p>
-    //     <p>Comment: ${post.comment}</p>
-    //     <p>Rating: ${post.rating}</p>
-    //     </div>
+  //   let postsHTML = "";
+  //   const posts = result.rows;
+  //   posts.map(
+  //     (post) =>
+  //       (postsHTML = `
+  //       <div class="post-container">
+  //       <p>Username: ${post.username}</p>
+  //       <p>Movie: ${post.movie_title}</p>
+  //       <p>Comment: ${post.comment}</p>
+  //       <p>Rating: ${post.rating}</p>
+  //       </div>
 
-    //     `.concat(postsHTML)) //so posts to at top of list not bottom
-    // );
-    // return postsHTML;
-  });
+  //       `.concat(postsHTML)) //so posts to at top of list not bottom
+  //   );
+  //   return postsHTML;
+  // });
 
   const form = `
     <form method="POST" action="/add-post">
-      <label for="username">Username:</label>
-      <input id="username" name="username" type="text" required />
-        <br />
       <label for="movie">Movie title:</label>
       <input id="movie" name="movie" type="text" />
         <br />
@@ -51,10 +48,7 @@ function showPosts(request, response) {
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link rel="stylesheet" type="text/css" href="styles.css" />
     <title>Team 1</title>
   </head>
   <body>
