b21bd4ab30b42148ec9d1d0100ca6a37b73661f3
Asmahan <82019270+AsmahanM@users.noreply.github.com>
2022-05-27 10:58:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ server.get("*", (req, res) => {
 
 server.post("/delete-posts", deletePost.post);
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
 
